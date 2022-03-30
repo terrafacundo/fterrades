10da22b99faf796cb5a6451d755f7bdb3f7da09d
disenofgti <terra.facundo.devacc@gmail.com>
2022-03-30 15:04:02 -0300

    Array aplicado, y metodos solucionados

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ class Dia {
         this.dia=dia;
         this.estimado=estimado;
         this.estudiado=estudiado;
-        this.ejer=ejer;
-        this.prod="";
+        this.ejercicio=ejer;
+        this.productividad="";
     }
 
     objetivoEstudio(){
@@ -34,16 +34,20 @@ class Dia {
         }
     }
     objetivoTreino(){
-        if(this.ejer=="si"){
-            this.ejer=true;
+        if(this.ejercicio=="si"){
+            this.ejercicio=true;
+            return true
         }
         else{
-            this.ejer=false;
+            this.ejercicio=false;
         }
     }
     pocaProductividad(){
-        if((objetivoEstudio==false)&&(objetivoTreino==false)){
-            this.prod="productivo";
+        if((this.objetivoEstudio()==true)&&(this.objetivoTreino()==true)){
+            this.productividad="productivo";
+        }
+        else{
+            this.productividad="no productivo";
         }
     }
 }
@@ -51,23 +55,15 @@ class Dia {
 
 
 console.log("Bienvenido al tracker de productividad. \nCon este tracker vas a poder hacer una evaluación simple de tu semana. Considerando:\nHoras de estudio/Horas programadas. \nEjercitación.");
-
-for (let i=1;i<2;i++){
-    semana.push(new Dia(
+let diaProductivo={}
+for (let i=1;i<8;i++){
+    diaProductivo=(new Dia(
     dia=i,
     horasEstimadas =Number(prompt(`¿Para el día ${i} cuánto desea estudiar?`)),
     horasEstudio =Number(prompt(`¿Para el día ${i} cuánto estudio?`)),
-    ejercicio = prompt(`¿Has ejercitado?`) ) )
+    ejercicio = prompt(`¿Has ejercitado?`) ))
+    
+    semana.push(diaProductivo);
+    diaProductivo.pocaProductividad();
 }
-
-console.log(Dia);
-console.log(semana);
-
-// let busquedaDia = prompt("¿Qué día te gustaria saber su productividad?");
-// if ( semana.find(Dia =>Dia.xe==busquedaDia))
-// {
-//     console.log("Su día fue productivo.")
-// }
-// else {
-//     console.log("Su dia no fue productivo.")
-// }
+console.log("Semana de productividad trackeada: ",semana);
